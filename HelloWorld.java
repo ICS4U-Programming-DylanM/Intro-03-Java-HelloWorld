1aa4d1d2912c87f6588ca0875fd6b73a882d56c7
Dylan Mutabazi <dylan.mutabazi@stu.ocsb.ca>
2025-09-08 14:36:48 +0000

    Added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+* This program says Hello world 
+*
+* @author Dylan Mutabazi
+* @version 1.0
+* @since September 2025
+*/
+
+class HelloWorld {
+    public static void main(String[]args){
+        // This program prints out " Hello world" 
+        System.out.println ("Hello world")
+    }
+}
